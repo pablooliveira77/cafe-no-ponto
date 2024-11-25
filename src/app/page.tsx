7bca47da4a16299cda928eaa11b3b5c84be4b974
pablo <pablolimadeoliveira77@gmail.com>
2024-11-25 10:26:42 -0300

    Mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -160,7 +160,7 @@ export default function Home() {
                       />
                     </div>
 
-                    <div id="cardapio" className="grid grid-cols-2 lg:grid-cols-3 gap-6">
+                    <div id="cardapio" className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 place-items-center">
                       {catalogo.map((item) => (
                         <ProductsInitial
                           key={item.id_catalogo}
